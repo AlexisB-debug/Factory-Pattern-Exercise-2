4a897acd7b61b8b4e928057c89a0052546997f6f
Alexis Boegner <alexisboegner@gmail.com>
2025-11-15 01:06:38 -0500

    Created Class Constructor MongoDataAccess

--- FactoryPatternExercise2/MongoDataAccess.cs
+++ FactoryPatternExercise2/MongoDataAccess.cs
@@ -2,6 +2,39 @@ namespace FactoryPatternExercise2;
 
 public class MongoDataAccess : IDataAccess
 {
+    List<Product> Products = new List<Product>();
+    public MongoDataAccess()
+    {
+        do
+        {
+            Console.WriteLine("Please, type the parcel to place it in the shopping cart or type 'checkout'!");
+            string input = Console.ReadLine();
+
+            if (input.ToLower() == "checkout")
+            {
+                break;
+            }
+
+            string parcel = input;
+            Console.WriteLine($"Please, type the price of {parcel}");
+
+            bool priceIsADecimal;
+            decimal price;
+
+            do
+            {
+                Console.WriteLine("The price is a figure");
+                priceIsADecimal = decimal.TryParse(Console.ReadLine(), out price);
+            }while(!priceIsADecimal);
+
+            Product newProduct = new Product()
+            {
+                Name = parcel,
+                Price = price
+            };
+            Products.Add(newProduct);
+        }while (true);
+    }
     public List<Product> LoadData()
     {
         Console.WriteLine($"I am reading data from {typeof(MongoDataAccess)}");
